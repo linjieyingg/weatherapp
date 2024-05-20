c9d5e6d507395d53a33a6b76f0b36bc6fe085062
linjieyingg <jlin41@stuy.edu>
2024-05-19 20:31:27 -0400

    models (for now)

--- project_folder/final/weather/views.py
+++ project_folder/final/weather/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+# from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Observation
+from django.db.models import Q
 
-# Create your views here.
+class ObservationListView(ListView):
+    model = Observation
+    context_object_name = 'observations'
+    template_name = 'weather/observation_list.html'
+    
+class ObservationDetailView(DetailView):
+    model = Observation
+    context_object_name = 'observation_details'
+    template_name = 'weather/observation_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        return context
