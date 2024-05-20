9d7a14fb1ef197436e31c8ed23e348363d4ed7cb
linjieyingg <jlin41@stuy.edu>
2024-05-19 20:44:46 -0400

    updated models

--- project_folder/final/weather/models.py
+++ project_folder/final/weather/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Observation(models.Model):
-    date = models.DateTimeField()
+    date = models.DateField()
     location = models.CharField()
     country = models.CharField()
     condition = models.CharField() ##cloud,rainy,sunny,etc
@@ -14,11 +14,11 @@ class Observation(models.Model):
     max_f = models.FloatField()
     min_c = models.FloatField()
     min_f = models.FloatField()
-    uv = models.SmallIntegerField()
     wind_mph = models.SmallIntegerField()
     wind_dir = models.CharField()
     pressure_in = models.SmallIntegerField()
     precip_in = models.SmallIntegerField()
+    
 
     class Meta:
         ordering = ['date']
@@ -26,7 +26,7 @@ class Observation(models.Model):
     def __str__(self):
     		return self.name
   
-class Forecast(models.Model):
+class Hourly(models.Model):
     date = models.DateTimeField()
     location = models.CharField()
     country = models.CharField()
@@ -34,15 +34,13 @@ class Forecast(models.Model):
     temp_c = models.FloatField()
     temp_f = models.FloatField()
     humidity = models.SmallIntegerField()
-    max_c = models.FloatField()
-    max_f = models.FloatField()
-    min_c = models.FloatField()
-    min_f = models.FloatField()
     uv = models.SmallIntegerField()
     wind_mph = models.SmallIntegerField()
     wind_dir = models.CharField()
     pressure_in = models.SmallIntegerField()
     precip_in = models.SmallIntegerField()
+    observation_id = models.ForeignKey(Observation, null=True, on_delete=models.SET_NULL)
+
 
     class Meta:
         ordering = ['date']
--- project_folder/final/weather/views.py
+++ project_folder/final/weather/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 # from django.urls import reverse_lazy
 from django.contrib import messages
-from .models import Observation
+from .models import Observation, Hourly
 from django.db.models import Q
 
 class ObservationListView(ListView):
@@ -19,4 +19,20 @@ class ObservationDetailView(DetailView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
+        return context
+    
+class HourlyListView(ListView):
+    model = Hourly
+    context_object_name = 'hourlies'
+    template_name = 'weather/hourly_list.html'
+    
+class HourlyDetailView(DetailView):
+    model = Hourly
+    context_object_name = 'hourly_details'
+    template_name = 'weather/hourly_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['observations'] = Observation.objects.filter(hourly_id=self.get_object())
         return context
