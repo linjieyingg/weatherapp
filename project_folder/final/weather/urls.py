e5b0eef1b8db9c1f7b1112741df12cf37c41c17e
linjieyingg <jlin41@stuy.edu>
2024-05-27 10:51:22 -0400

    s

--- project_folder/final/weather/urls.py
+++ project_folder/final/weather/urls.py
@@ -8,6 +8,6 @@ app_name = "weather"
 urlpatterns = [
     path('', views.ObservationListView.as_view(template_name='weather/weather_list.html'), name="weather_list"),
     path('<int:pk>', views.ObservationDetailView.as_view(), name="weather_detail"),
-    path('<int:weather_id>/hourly/', HourlyListView.as_view(), name='hourly_list'),
+    # path('<int:weather_id>/hourly/', HourlyListView.as_view(), name='hourly_list'),
     # path('song_search',  views.SearchSongsListView.as_view(template_name='songs/song_search.html'), name="song_search"),
 ]
