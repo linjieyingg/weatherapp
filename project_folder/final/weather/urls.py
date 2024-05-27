0521f934578b201b7ec9b16df940bad3d5652884
linjieyingg <jlin41@stuy.edu>
2024-05-27 19:47:10 -0400

    s

--- project_folder/final/weather/urls.py
+++ project_folder/final/weather/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:pk>', views.ObservationDetailView.as_view(), name="weather_detail"),
     path("bis/<int:pk>", views.ObservationDetailbisView.as_view(), name="weather_detail_bis"),
     path("js/<int:pk>", views.ObservationDetailJsView.as_view(), name="weather_detail_js"),
-    path("update_bis/<int:pk>", views.ObservationUpdatebisView.as_view(), name="weather_update"),
+    path("update_bis/<int:pk>", views.ObservationUpdatebisView.as_view(), name="weather_update_bis"),
+    path("update/<int:pk>", views.ObservationUpdateView.as_view(), name="weather_update"),
 
 ]
